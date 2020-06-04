9fa440b389af27750c990ca68e94b7f20e185086
Arom Jhee <aromjhee@gmail.com>
2020-06-04 10:46:07 -0700

    update bin/www file and config/database.js to test connection to db

--- config/database.js
+++ config/database.js
@@ -5,33 +5,6 @@ const {
   host,
 } = require('./index').db;
 
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  }
-}
-
 module.exports = {
   development: {
     username,
@@ -41,3 +14,31 @@ module.exports = {
     dialect: 'postgres',
   },
 };
+
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   }
+// }
