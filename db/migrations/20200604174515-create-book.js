279d5e703d8185415a6dc397ae1da72dee443802
Arom Jhee <aromjhee@gmail.com>
2020-06-04 10:48:25 -0700

    create migration and model file for Books table

---
+++ db/migrations/20200604174515-create-book.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      releaseDate: {
+        type: Sequelize.DATEONLY
+      },
+      pageCount: {
+        type: Sequelize.INTEGER
+      },
+      publisher: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Books');
+  }
+};
