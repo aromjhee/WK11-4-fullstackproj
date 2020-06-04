ea0fde92997a5aa7f8afabb2a93d02311dedb348
Arom Jhee <aromjhee@gmail.com>
2020-06-04 13:23:55 -0700

    update routes.js to implement express-validator

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const csrf = require('csurf');
+const { check, validationResult } = require('express-validator');
 
 const db = require('./db/models');
 
@@ -8,6 +9,34 @@ const router = express.Router();
 const csrfProtection = csrf({ cookie: true });
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+const bookValidators = [
+  check('title')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Title')
+    .isLength({ max: 255 })
+    .withMessage('Title must not be more than 255 characters long'),
+  check('author')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Author')
+    .isLength({ max: 100 })
+    .withMessage('Author must not be more than 100 characters long'),
+  check('releaseDate')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Release Date')
+    .isISO8601()
+    .withMessage('Please provide a valid date for Release Date'),
+  check('pageCount')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Page Count')
+    .isInt({ min: 0 })
+    .withMessage('Please provide a valid integer for Page Count'),
+  check('publisher')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Publisher')
+    .isLength({ max: 100 })
+    .withMessage('Publisher must not be more than 100 characters long'),
+];
 
 router.get('/', asyncHandler(async (req, res) => {
   // throw new Error('This is a test error');
@@ -26,7 +55,7 @@ router.get('/book/add', csrfProtection, (req, res) => {
   });
 });
 
-router.post('/book/add', csrfProtection, asyncHandler(async (req, res, next) => {
+router.post('/book/add', csrfProtection, bookValidators, asyncHandler(async (req, res, next) => {
   const {
     title,
     author,
@@ -43,22 +72,37 @@ router.post('/book/add', csrfProtection, asyncHandler(async (req, res, next) => 
     publisher,
   });
 
-  try {
+  const validatorError = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
     await book.save();
     res.redirect('/');
-  } catch (err) {
-    if (err.name === 'SequelizeValidationError') {
-      const errors = err.errors.map(error => error.message);
-      res.render('book-add', {
-        title: 'Add Book',
-        book,
-        errors,
-        csrfToken: req.csrfToken(),
-      });
-    } else {
-      next(err);
-    }
+  } else {
+    const errors = validatorErrors.array().map(error => error.msg);
+    res.render('book-add', {
+      title: 'Add Book',
+      book,
+      errors,
+      csrfToken: req.csrfToken(),
+    })
   }
+
+  // try {
+  //   await book.save();
+  //   res.redirect('/');
+  // } catch (err) {
+  //   if (err.name === 'SequelizeValidationError') {
+  //     const errors = err.errors.map(error => error.message);
+  //     res.render('book-add', {
+  //       title: 'Add Book',
+  //       book,
+  //       errors,
+  //       csrfToken: req.csrfToken(),
+  //     });
+  //   } else {
+  //     next(err);
+  //   }
+  // }
 }));
 
 
