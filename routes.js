a1aaeee5e89549f9eafa736b75c09a2fe015ee23
Arom Jhee <aromjhee@gmail.com>
2020-06-04 11:05:43 -0700

    in routes.js: import db/models and update home route to render book titles and also update index.pug to display them

--- routes.js
+++ routes.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
+const db = require('./db/models');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res, next) => {
   // throw new Error('This is a test error');
-  res.render('index', { title: 'Home' });
+
+  try {
+    const books = await db.Book.findAll({order: [['title', 'ASC']]});
+    res.render('index', { title: 'Home', books });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
